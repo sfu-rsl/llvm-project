5bd6182097abeed0fe1e3b69e0d01f48b5d5800b
Frédéric Tuong <ftuong@sfu.ca>
2022-03-17 20:26:05 -0700

    register the pass (synchronize with llvm/lib/Transforms)

--- llvm/lib/Transforms/Utils/Utils.cpp
+++ llvm/lib/Transforms/Utils/Utils.cpp
@@ -38,6 +38,7 @@ void llvm::initializeTransformUtils(PassRegistry &Registry) {
   initializeNameAnonGlobalLegacyPassPass(Registry);
   initializePromoteLegacyPassPass(Registry);
   initializeStripNonLineTableDebugLegacyPassPass(Registry);
+  initializeSymbolizeLegacyPassPass(Registry);
   initializeUnifyFunctionExitNodesLegacyPassPass(Registry);
   initializeMetaRenamerPass(Registry);
   initializeStripGCRelocatesLegacyPass(Registry);
